10fd8252908b8d1545faa95d399d025f00f326df
ydmnsky <yaroslavdomansky@yandex.ru>
2022-04-20 17:01:22 +0300

    realised RecordListFragment.kt with RecyclerView

--- app/src/main/java/com/example/mentalhealthdairy/RecordListFragment.kt
+++ app/src/main/java/com/example/mentalhealthdairy/RecordListFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +17,7 @@ private const val TAG = "RecordListFragment"
 class RecordListFragment: Fragment() {
 
     private lateinit var recordRecyclerView: RecyclerView
+    private var adapter: RecordAdapter ?= null
 
     private val recordListViewModel: RecordListViewModel by viewModels()
 
@@ -33,7 +36,56 @@ class RecordListFragment: Fragment() {
         recordRecyclerView = view.findViewById(R.id.record_recycler_view) as RecyclerView
         recordRecyclerView.layoutManager = LinearLayoutManager(context)
 
+        updateUI()
+
         return view
+    }
+
+    private inner class RecordHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var record: Record
+        private val titleTextView: TextView = itemView.findViewById(R.id.record_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.record_date)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(record: Record) {
+            this.record = record
+            titleTextView.text = record.title
+            dateTextView.text = record.date.toString()
+        }
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(
+                context,
+                "${record.title} is pressed",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private inner class RecordAdapter(var records: List<Record>) : RecyclerView.Adapter<RecordHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_record, parent, false)
+            return RecordHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return records.size
+        }
+
+        override fun onBindViewHolder(holder: RecordHolder, position: Int) {
+            val record = records[position]
+            holder.bind(record)
+        }
+
+    }
+
+    private fun updateUI() {
+        val records = recordListViewModel.records
+        adapter = RecordAdapter(records)
+        recordRecyclerView.adapter = adapter
     }
 
 
